a27db48cb135d10a82bbb059ab0e4cf422128ce8
GustavoIWN <137819908+GustavoIWN@users.noreply.github.com>
2024-10-18 14:18:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Game',
+    'Melhor jogo?',
+    'Minicraft.'
+)
+
+criaCartao(
+    'Conhecimento',
+    'Melhor IA?',
+    'Depende tem o Chat gpt, Gemini, Copilot.'
+)
+
+criaCartao(
+    'Comida',
+    'Melhor comida?',
+    'Tem a Pizza e o hambúrgue por causa da diversidade de combinações.'
+)
+
+criaCartao(
+    'Hipótese',
+    'quem veio primeiro o ovo ou a galinha?',
+    'Ovo.'
+)
